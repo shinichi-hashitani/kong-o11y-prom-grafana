6dccee3a13bc37071d2814b0ee5da5ed2f96df6b
Shinichi Hashitani <hashitani@icloud.com>
2024-08-26 19:38:42 +0900

    Initial commit

---
+++ scripts/plugins.sh
@@ -0,0 +1,15 @@
+curl -X POST http://kong-gateway:8001/plugins \
+    --header "accept: application/json" \
+    --header "Content-Type: application/json" \
+    --data '
+      {
+        "name": "prometheus",
+        "config": {
+          "per_consumer": true,
+          "status_code_metrics": true,
+          "latency_metrics": true,
+          "bandwidth_metrics": true,
+          "upstream_health_metrics": true
+        }
+      }
+    '
